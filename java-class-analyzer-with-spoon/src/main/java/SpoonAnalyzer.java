36d715fc7b49db9f54338383a5464c5b69a39ee6
rugii913 <rugii913@gmail.com>
2024-08-30 17:34:51 +0900

    chore: SLF4J 구현체 에러 임시 조치(slf4j-simple 종속성 추가 등)
    SLF4J 구현체 에러 임시 조치
    - 다음의 에러에 대한 임시 조치
      - SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
      - SLF4J: Defaulting to no-operation (NOP) logger implementation
      - SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
    - slf4j-simple 종속성 추가 및 로그 레벨 설정 등 작업

--- java-class-analyzer-with-spoon/src/main/java/SpoonAnalyzer.java
+++ java-class-analyzer-with-spoon/src/main/java/SpoonAnalyzer.java
@@ -1,3 +1,5 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spoon.Launcher;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtField;
@@ -5,10 +7,11 @@ import spoon.reflect.declaration.CtMethod;
 
 public class SpoonAnalyzer {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpoonAnalyzer.class);
+
     public static void main(String[] args) {
         // 기초적인 사용법 https://spoon.gforge.inria.fr/launcher.html
         // API docs https://spoon.gforge.inria.fr/mvnsites/spoon-core/apidocs/
-
         CtClass<?> ctClass = Launcher.parseClass("""
                 class TestClass {
                     private String testField1 = "This is default expression of testField1.";
@@ -18,22 +21,22 @@ public class SpoonAnalyzer {
                 }
                 """);
 
-        System.out.println("ctClass.getSimpleName(): " + ctClass.getSimpleName());
+        logger.debug("ctClass.getSimpleName(): {}", ctClass.getSimpleName());
 
         for (CtField<?> field : ctClass.getFields()) {
-            System.out.println("field.getVisibility(): " + field.getVisibility());
-            System.out.println("field.getType(): " + field.getType());
-            System.out.println("field.getSimpleName(): " + field.getSimpleName());
-            System.out.println("field.getDefaultExpression(): " + field.getDefaultExpression());
+            logger.debug("field.getVisibility(): {}", field.getVisibility());
+            logger.debug("field.getType(): {}", field.getType());
+            logger.debug("field.getSimpleName(): {}", field.getSimpleName());
+            logger.debug("field.getDefaultExpression(): {}", field.getDefaultExpression());
         }
 
         for (CtMethod<?> method : ctClass.getMethods()) {
-            System.out.println("method.getSignature(): " + method.getSignature());
-            System.out.println("method.getVisibility(): " + method.getVisibility());
-            System.out.println("method.getType(): " + method.getType());
-            System.out.println("method.getSimpleName(): " + method.getSimpleName());
-            System.out.println("method.getParameters(): " + method.getParameters());
-            System.out.println("method.getBody(): " + method.getBody());
+            logger.debug("method.getSignature(): {}", method.getSignature());
+            logger.debug("method.getVisibility(): {}", method.getVisibility());
+            logger.debug("method.getType(): {}", method.getType());
+            logger.debug("method.getSimpleName(): {}", method.getSimpleName());
+            logger.debug("method.getParameters(): {}", method.getParameters());
+            logger.debug("method.getBody(): {}", method.getBody());
         }
     }
 }
