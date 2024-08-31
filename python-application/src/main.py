fde8caa31b24b699281275e8fbe248745257ddea
rugii913 <rugii913@gmail.com>
2024-09-01 01:33:11 +0900

    refactor: 객체를 사용하는 코드로 리팩토링-특정 객체에서만 JPype 사용
    객체를 사용하는 코드로 리팩토링 - 특정 객체에서만 JPype 사용
    - ClassAnalyzerConnector만이 JPype를 사용하도록 함
      - ClassAnalyerConnector는 싱글톤을 보장하고자 함

--- python-application/src/main.py
+++ python-application/src/main.py
@@ -1,24 +1,11 @@
-import jpype
-import jpype.imports
-from jpype.types import *
+from service.class_analyzer_connector import ClassAnalyzerConnector
 
 
 def main():
-    jpype.startJVM(
-        jvmpath=jpype.getDefaultJVMPath(),
-        classpath="jars/class-analyzer-0.1-SNAPSHOT.jar",
-    )
-    print(jpype.isJVMStarted())
-
-    ClassAnalyzerController = jpype.JClass("io.classanalyzer.ClassAnalyzerController")
-
-    analyzer = ClassAnalyzerController()
-
-    analyzer.analyze("jars/java-class-analyzer-with-spoon-1.0-SNAPSHOT.jar")
-
-    # 디컴파일된 코드 삭제 작업
-    analyzer.cleanDecompiled()
-    jpype.shutdownJVM()
+    classAnalyzerConnector = ClassAnalyzerConnector()
+    analyzed_model = classAnalyzerConnector.analyze("jars\java-class-analyzer-with-spoon-1.0-SNAPSHOT.jar")
+    classAnalyzerConnector.cleanDecompiled()
+    classAnalyzerConnector.shutdown()
 
 
 if __name__ == "__main__":
