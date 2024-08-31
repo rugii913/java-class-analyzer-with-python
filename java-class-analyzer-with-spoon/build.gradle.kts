294e97e966a42cc055823610cf179e9d16740be5
rugii913 <rugii913@gmail.com>
2024-08-31 15:46:42 +0900

    chore: Spoon Decompiler, Apache Commons IO 종속성 추가
    Spoon Decompiler, Apache Commons IO 종속성 추가
    - Spoon의 JarLauncher를 활용하기 위해 Spoon Decompiler 종속성 추가
    - 파일 시스템 접근을 간단히 처리하기 위해 Apache Commons IO 종속성 추가

--- java-class-analyzer-with-spoon/build.gradle.kts
+++ java-class-analyzer-with-spoon/build.gradle.kts
@@ -10,18 +10,21 @@ repositories {
 }
 
 dependencies {
-    // spoon-core 종속성 추가
-    implementation("fr.inria.gforge.spoon:spoon-core:10.4.1")
+    // Spoon Core 종속성
+    implementation("fr.inria.gforge.spoon:spoon-core:11.0.0")
+    // Spoon Decompiler 종속성 - JarLauncher를 사용하기 위함
+    implementation("fr.inria.gforge.spoon:spoon-decompiler:0.1.0")
 
-    // slf4j-simple 종속성 추가
+    // Apache Commons IO 종속성 - 파일 처리
+    implementation("commons-io:commons-io:2.16.1")
+
+    // SLF4J Simple Provider 종속성 - SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder" 오류에 대한 임시 조치
     /*
-    * SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder". 임시 조치
     * - 참고 자료
     *   - https://www.slf4j.org/codes.html#StaticLoggerBinder, https://www.slf4j.org/manual.html, https://github.com/qos-ch/slf4j/blob/master/README.md
     *   - https://stackoverflow.com/questions/14544991/how-to-configure-slf4j-simple
     * */
-    // (cf.) implementation("org.slf4j:slf4j-api:2.0.16")는 → spoon-core에 포함됨
-    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("org.slf4j:slf4j-simple:2.0.16") // (cf.) implementation("org.slf4j:slf4j-api:2.0.16")는 spoon-core에 포함됨
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
