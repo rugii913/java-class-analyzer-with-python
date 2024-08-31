488789be8ca7aa2d5f07a4aaf85edf7fea7b5641
rugii913 <rugii913@gmail.com>
2024-08-31 16:08:38 +0900

    docs: 주석 정리
    주석 정리
    - 필요 없는 임시 코드 주석 처리된 부분 제거
    - 참고 자료 주석만 남김

--- java-class-analyzer-with-spoon/src/main/java/io/analyzer/MainController.java
+++ java-class-analyzer-with-spoon/src/main/java/io/analyzer/MainController.java
@@ -15,6 +15,8 @@ public class MainController {
     private final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     public void control() {
+        // 기초적인 사용법 https://spoon.gforge.inria.fr/launcher.html
+        // API docs https://spoon.gforge.inria.fr/mvnsites/spoon-core/apidocs/
         File targetJarPath = new File("target-jar\\java-class-analyzer-with-spoon-1.0-SNAPSHOT.jar");
         File decompiledSourceCodePath = new File("target-jar\\out");
 
@@ -35,36 +37,5 @@ public class MainController {
             logger.error("decompile 파일 삭제 과정 중 오류", e);
             throw new RuntimeException(e);
         }
-
-        // 기초적인 사용법 https://spoon.gforge.inria.fr/launcher.html
-        // API docs https://spoon.gforge.inria.fr/mvnsites/spoon-core/apidocs/
-        /*
-        CtClass<?> ctClass = Launcher.parseClass("""
-                class TestClass {
-                    private String testField1 = "This is default expression of testField1.";
-                
-                    public void testMethod1() { System.out.println("test method 1"); }
-                    public void testMethod2() { System.out.println("test method 2"); }
-                }
-                """);
-
-        logger.debug("ctClass.getSimpleName(): {}", ctClass.getSimpleName());
-
-        for (CtField<?> field : ctClass.getFields()) {
-            logger.debug("field.getVisibility(): {}", field.getVisibility());
-            logger.debug("field.getType(): {}", field.getType());
-            logger.debug("field.getSimpleName(): {}", field.getSimpleName());
-            logger.debug("field.getDefaultExpression(): {}", field.getDefaultExpression());
-        }
-
-        for (CtMethod<?> method : ctClass.getMethods()) {
-            logger.debug("method.getSignature(): {}", method.getSignature());
-            logger.debug("method.getVisibility(): {}", method.getVisibility());
-            logger.debug("method.getType(): {}", method.getType());
-            logger.debug("method.getSimpleName(): {}", method.getSimpleName());
-            logger.debug("method.getParameters(): {}", method.getParameters());
-            logger.debug("method.getBody(): {}", method.getBody());
-        }
-        */
     }
 }
