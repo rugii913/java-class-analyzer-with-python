485e8f4ad2aa6c89aac5982b8375dd7b61d99d39
rugii913 <rugii913@gmail.com>
2024-09-01 00:31:51 +0900

    add: python JPype 라이브러리 동작 확인
    python JPype 라이브러리 동작 확인

--- python-application/src/main.py
+++ python-application/src/main.py
@@ -1,5 +1,25 @@
+import jpype
+import jpype.imports
+from jpype.types import *
+
+
 def main():
-  print("Hello world!")
+    jpype.startJVM(
+        jvmpath=jpype.getDefaultJVMPath(),
+        classpath="jars/class-analyzer-0.1-SNAPSHOT.jar",
+    )
+    print(jpype.isJVMStarted())
+
+    ClassAnalyzerController = jpype.JClass("io.classanalyzer.ClassAnalyzerController")
+
+    analyzer = ClassAnalyzerController()
+
+    analyzer.analyze("jars/java-class-analyzer-with-spoon-1.0-SNAPSHOT.jar")
+
+    # 디컴파일된 코드 삭제 작업
+    analyzer.cleanDecompiled()
+    jpype.shutdownJVM()
+
 
 if __name__ == "__main__":
-  main()
+    main()
