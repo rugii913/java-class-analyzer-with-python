5c8f2749458742b4da6c5b93042b01153f3e025a
rugii913 <rugii913@gmail.com>
2024-09-06 23:33:12 +0900

    fix: AnalyzerAdapter 추가, ClassAnalyzerConnector와 GUI를 연결하는 역할
    AnalyzerAdapter 추가, ClassAnalyzerConnector와 GUI를 연결하는 역할
    - AnalyzerAdapter
      - GUI(MainWindow)에서 바로 ClassAnalyzerConnector를 호출하지 않고, AnalyzerAdapter에게 위임
    - main.py에 있던 ClassAnalyzerConnector 인스턴스 호출 로직과 ClassDefinitionHwpTableWriter 호출 로직을 AnalyzerAdapter로 옮김
    * 추후 전체 구조 다시 고려 필요
    
    - gitignore에 analyzer/** 추가
      - 사용할 JVM 라이브러리의 위치를 analyzer 디렉토리로 옮김

--- python-application/src/gui/main_window.py
+++ python-application/src/gui/main_window.py
@@ -1,21 +1,27 @@
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QFileDialog, QLineEdit, QPushButton, QWidget, QHBoxLayout
 
+from service.analyzer_adapter import AnalyzerAdapter
+
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.__analyzer_adapter = AnalyzerAdapter()
         self.initialize_main_window()
 
-        self.import_button = QPushButton("file select", self)
-        self.import_button.clicked.connect(self.selectFile)
         self.line_edit = QLineEdit(self)
         self.line_edit.setFixedWidth(480)
         self.line_edit.setDisabled(True)
+        self.file_select_button = QPushButton("file select", self)
+        self.file_select_button.clicked.connect(self.selectFile)
+        self.analyze_button = QPushButton("analyze", self)
+        self.analyze_button.clicked.connect(lambda: self.analyze(self.line_edit.text())) # arg를 넘겨 호출하기 위해 lambda 사용
 
         hbox = QHBoxLayout()
         hbox.addWidget(self.line_edit)
-        hbox.addWidget(self.import_button)
+        hbox.addWidget(self.file_select_button)
         hbox.addStretch(1)
 
         self.setLayout(hbox)
@@ -31,6 +37,9 @@ class MainWindow(QWidget):
         )  # 기본 디렉토리 지정할 경우 directory=""설정
         if is_selected:
             self.line_edit.setText(file_path)
+
+    def analyze(self, target_jar_path):
+        self.__analyzer_adapter.analyze(target_jar_path)
 
     # def setup_main_window(self):
     #     hello_lable = QLabel(self)
--- python-application/src/main.py
+++ python-application/src/main.py
@@ -3,9 +3,6 @@ import sys
 from PyQt6.QtWidgets import QApplication
 
 from gui.main_window import MainWindow
-from service.class_analyzer_connector import ClassAnalyzerConnector
-from service.hwp_connector import HwpConnector
-from service.table_writer import ClassDefinitionHwpTableWriter
 
 
 def main():
@@ -14,18 +11,6 @@ def main():
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
-    # 일부러 코드에 접근할 수 없는 위치로 옮김
-    classAnalyzerConnector = ClassAnalyzerConnector()
-    print("분석할 jar 파일의 경로를 입력하세요. >>> ", end = "") # (ex.) C:\toy-project\java-class-analyzer-with-python\python-application\jars\class-analyzer-ex.jar
-    target_jar_path = input()
-    analyzed_model = classAnalyzerConnector.analyze(target_jar_path)
-
-    table_writer = ClassDefinitionHwpTableWriter(HwpConnector(), analyzed_model)
-    table_writer.writeClassDefinitionTables()
-
-    classAnalyzerConnector.cleanDecompiled()
-    classAnalyzerConnector.shutdown()
 
 
 if __name__ == "__main__":
