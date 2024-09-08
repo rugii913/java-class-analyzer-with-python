f1117b40dfe8134fd4fed02426e5b3ae33f14304
rugii913 <rugii913@gmail.com>
2024-09-08 22:08:53 +0900

    test: (테스트 실패)methods 리스트에 메서드 선언 순서대로 메서드가 들어가는지 확인하는 테스트 작성
    methods 리스트에 메서드 선언 순서대로 메서드가 들어가는지 확인하는 테스트 작성
    - Spoon의 기본 동작은 클래스 분석 후 메서드를 선언 순서가 아닌 메서드 이름 lexicographic 순서로 저장하므로 실패할 수밖에 없는 테스트

--- class-analyzer/src/test/java/io/classanalyzer/service/TypeSimpleInfoMakerTest.kt
+++ class-analyzer/src/test/java/io/classanalyzer/service/TypeSimpleInfoMakerTest.kt
@@ -11,30 +11,68 @@ import java.io.FileWriter
 class TypeSimpleInfoMakerTest {
 
     private val typeSimpleInfoProvider = TypeSimpleInfoProvider()
-    private val targetClassText = """
-        class ClassForTest {
+    private val targetClassTextWithGenericType = """
+        class ClassWithGenericType {
             public List<String> fieldTest = null;
             public Set<Map<String, String>> methodTest(String[] arg1, Map<Long, String> arg2) {
                 return null;
             }
         }
     """.trimIndent()
-    private val targetClassCtModel: CtModel
+    private val ctModelWithGenericType: CtModel
+
+    private val targetClassTextWithManyMethods = """
+        class ClassWithGenericType {
+            public void firstMethod() {
+                return null;
+            }
+            
+            public void secondMethod() {
+                return null;
+            }
+            
+            public void thirdMethod() {
+                return null;
+            }
+            
+            public void fourthMethod() {
+                return null;
+            }
+            
+            public void fifthMethod() {
+                return null;
+            }
+        }
+    """.trimIndent()
+    private val ctModelWithManyMethods: CtModel
 
     init {
-        val tempFile = File.createTempFile("ClassForTest", ".java")
-        FileWriter(tempFile).use { writer -> writer.write(targetClassText) } // (cf.) use()는 let()과 비슷한데, Closeable에 대해 호출하는 함수
+        val tempFile1 = File.createTempFile("ClassWithGenericType", ".java")
+        FileWriter(tempFile1).use { writer -> writer.write(targetClassTextWithGenericType) } // (cf.) use()는 let()과 비슷한데, Closeable에 대해 호출하는 함수
+        ctModelWithGenericType = Launcher().also { it.addInputResource(tempFile1.absolutePath) }.buildModel()
 
-        targetClassCtModel = Launcher().also { it.addInputResource(tempFile.absolutePath) }.buildModel()
+        val tempFile2 = File.createTempFile("ClassWithManyMethods", ".java")
+        FileWriter(tempFile2).use { writer -> writer.write(targetClassTextWithManyMethods) }
+        ctModelWithManyMethods = Launcher().also { it.addInputResource(tempFile2.absolutePath) }.buildModel()
     }
 
     @Test
     fun testProvideTypeSimpleInfoList() {
-        val typeSimpleInformation = typeSimpleInfoProvider.provideTypeSimpleInfoList(targetClassCtModel)[0]
+        val typeSimpleInformation = typeSimpleInfoProvider.provideTypeSimpleInfoList(ctModelWithGenericType)[0]
 
-        assertThat(typeSimpleInformation.simpleName).isEqualTo("ClassForTest")
+        assertThat(typeSimpleInformation.simpleName).isEqualTo("ClassWithGenericType")
         assertThat(typeSimpleInformation.fields[0].type).isEqualTo("List<String>")
         assertThat(typeSimpleInformation.methods[0].parameterTypeList).isEqualTo(listOf("String[]", "Map<Long, String>"))
         assertThat(typeSimpleInformation.methods[0].returnType).isEqualTo("Set<Map<String, String>>")
+    }
+
+    @Test
+    fun testClassWithManyMethods() {
+        val typeSimpleInformation = typeSimpleInfoProvider.provideTypeSimpleInfoList(ctModelWithManyMethods)[0]
+        assertThat(typeSimpleInformation.methods[0].simpleName).isEqualTo("firstMethod")
+        assertThat(typeSimpleInformation.methods[1].simpleName).isEqualTo("secondMethod")
+        assertThat(typeSimpleInformation.methods[2].simpleName).isEqualTo("thirdMethod")
+        assertThat(typeSimpleInformation.methods[3].simpleName).isEqualTo("fourthMethod")
+        assertThat(typeSimpleInformation.methods[4].simpleName).isEqualTo("fifthMethod")
     }
 }
