21f57ccab4a008daa933feb51d3a015ee53ec504
rugii913 <rugii913@gmail.com>
2024-09-08 21:53:36 +0900

    feat: 복잡한 CtModel, CtType 정보를 간단하게 바꿔주고, generic type 정보를 평탄화하는 로직 작성
    - 복잡한 CtModel, CtType 정보를 간단하게 바꾸는 로직
      - 복잡한 CtModel 데이터의 구조를 List<TypeSimpleInfo>로 간단하게 바꿈
      - 복잡한 CtType 구조를 simpleName, fields: List<FieldSimpleInfo>, methods: List<MethodSimpleInfo>로 간단하게 바꿈
    
    - generic type 정보를 평탄화
      - flattenCtTypeReference() 메서드에서 generic type 정보를 String으로 평탄화
      - generic type이 또 다시 generic type을 갖고 있는 경우 재귀적으로 평탄화

--- class-analyzer/src/main/java/io/classanalyzer/service/TypeSimpleInfoProvider.kt
+++ class-analyzer/src/main/java/io/classanalyzer/service/TypeSimpleInfoProvider.kt
@@ -7,6 +7,7 @@ import spoon.reflect.CtModel
 import spoon.reflect.declaration.CtField
 import spoon.reflect.declaration.CtMethod
 import spoon.reflect.declaration.CtType
+import spoon.reflect.reference.CtTypeReference
 
 class TypeSimpleInfoProvider {
 
@@ -15,21 +16,42 @@ class TypeSimpleInfoProvider {
     }
 
     private fun <T> refineToTypeSimpleInfo(ctType: CtType<T>): TypeSimpleInfo {
-        val fields = ctType.fields.map { refineToFieldSimpleInfo(it) }
-        val methods = ctType.methods.map { refineToMethodSimpleInfo(it) }
+        val refinedFields = ctType.fields.map { refineToFieldSimpleInfo(it) }
+        val refinedMethods = ctType.methods.map { refineToMethodSimpleInfo(it) }
 
         return TypeSimpleInfo(
             simpleName = ctType.simpleName,
-            fields = fields,
-            methods = methods,
+            fields = refinedFields,
+            methods = refinedMethods,
         )
     }
 
-    private fun <T> refineToFieldSimpleInfo(ctField: CtField<T>): FieldSimpleInfo {
-        return FieldSimpleInfo("", "", "")
-    }
+    private fun <T> refineToFieldSimpleInfo(ctField: CtField<T>) =
+        FieldSimpleInfo(
+            simpleName = ctField.simpleName,
+            visibility = ctField.visibility.name,
+            type = flattenCtTypeReference(ctField.type),
+        )
 
-    private fun <T> refineToMethodSimpleInfo(ctMethod: CtMethod<T>): MethodSimpleInfo {
-        return MethodSimpleInfo("", "", listOf(""), "")
+    private fun <T> refineToMethodSimpleInfo(ctMethod: CtMethod<T>) =
+        MethodSimpleInfo(
+            simpleName = ctMethod.simpleName,
+            visibility = ctMethod.visibility.name,
+            parameterTypeList = ctMethod.parameters.map { flattenCtTypeReference(it.type) },
+            returnType = flattenCtTypeReference(ctMethod.type),
+        )
+
+    companion object GenericTypeFlattener {
+        fun <T> flattenCtTypeReference(ctTypeReference: CtTypeReference<T>): String {
+            val genericTypeList = ctTypeReference.actualTypeArguments
+            // (cf.) generic type의 CtTypeReference에 대해서도 재귀적으로 평탄화시킴
+            val flattenedGenericType = genericTypeList.joinToString(", ") { flattenCtTypeReference(it) }
+
+            return if (genericTypeList.isNotEmpty()) {
+                "${ctTypeReference.simpleName}<$flattenedGenericType>"
+            } else {
+                ctTypeReference.simpleName
+            }
+        }
     }
 }
