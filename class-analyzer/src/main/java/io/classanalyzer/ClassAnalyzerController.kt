f7c11a8b9d003b9116d4d9dc54ca983df85f6dcb
rugii913 <rugii913@gmail.com>
2024-09-09 00:43:07 +0900

    fix: 분석기 컨트롤러에서 TypeSimpleInfoProvider 인스턴스의 작업 결과물을 return 하도록 함
    ClassAnalyzerController에서 TypeSimpleInfoProvider 인스턴스의 작업 결과물을 return 하도록 함
    - 이에 따라 Python 쪽 로직도 수정 필요

--- class-analyzer/src/main/java/io/classanalyzer/ClassAnalyzerController.kt
+++ class-analyzer/src/main/java/io/classanalyzer/ClassAnalyzerController.kt
@@ -1,42 +1,24 @@
 package io.classanalyzer
 
-import org.slf4j.Logger
+import io.classanalyzer.dto.TypeSimpleInfo
+import io.classanalyzer.service.TypeSimpleInfoProvider
 import org.slf4j.LoggerFactory
 import spoon.JarLauncher
-import spoon.reflect.CtModel
-import spoon.reflect.declaration.CtType
 import java.io.File
 import java.io.IOException
 
 class ClassAnalyzerController {
 
-    private val logger: Logger = LoggerFactory.getLogger(ClassAnalyzerController::class.java)
+    private val logger = LoggerFactory.getLogger(ClassAnalyzerController::class.java)
     private val decompiledCodeDirectory = File("temp\\decompiled")
+    private val typeSimpleInfoProvider = TypeSimpleInfoProvider()
 
-    fun analyze(targetJarPath: String): CtModel {
+    fun analyze(targetJarPath: String): List<TypeSimpleInfo> {
         // 기초적인 사용법 https://spoon.gforge.inria.fr/launcher.html
         // API docs https://spoon.gforge.inria.fr/mvnsites/spoon-core/apidocs/
         val jarLauncher = JarLauncher(targetJarPath, decompiledCodeDirectory.path)
         jarLauncher.buildModel()
-        val model = jarLauncher.model
-        /*
-        for (targetType in model.allTypes) {
-            targetType.declaredFields.forEach {
-                val fieldDeclaration = it.fieldDeclaration
-                println(fieldDeclaration.getSimpleName())
-                println(fieldDeclaration.getVisibility())
-                println(fieldDeclaration.getType().simpleName)
-            }
-
-            targetType.methods.forEach {
-                println(it.getSimpleName())
-                println(it.getVisibility())
-                println(it.getParameters().map { it.getType().getSimpleName() })
-                println(it.getType())
-            }
-        }
-*/
-        return model
+        return typeSimpleInfoProvider.provideTypeSimpleInfoList(jarLauncher.model)
     }
 
     fun cleanDecompiled() {
@@ -49,9 +31,4 @@ class ClassAnalyzerController {
             throw RuntimeException(e)
         }
     }
-    /*
-    fun test(test1: String, test2: List<Map<String, String>>) {
-        return
-    }
-    */
 }
