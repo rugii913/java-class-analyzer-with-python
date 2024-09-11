eeefd471805a25a9c5f49031ff9ee0542f3f1165
rugii913 <rugii913@gmail.com>
2024-09-11 21:07:22 +0900

    fix: 명시되지 않은 가시성을 처리해서 String을 return하는 로직 추가
    implicit visibility(명시되지 않은 가시성)을 처리해서 String을 return하는 로직 추가
    - private 메서드 processImplicitVisibility() 분석 대상 타입이 interface인지 아닌지에 따라 명시되지 않은 가시성을 처리
      - interface인 경우 "public"을 return
      - interface가 아닌 경우 package를 return

--- class-analyzer/src/main/java/io/classanalyzer/service/TypeSimpleInfoProvider.kt
+++ class-analyzer/src/main/java/io/classanalyzer/service/TypeSimpleInfoProvider.kt
@@ -7,6 +7,7 @@ import spoon.reflect.CtModel
 import spoon.reflect.declaration.CtField
 import spoon.reflect.declaration.CtMethod
 import spoon.reflect.declaration.CtType
+import spoon.reflect.declaration.ModifierKind
 import spoon.reflect.reference.CtTypeReference
 
 class TypeSimpleInfoProvider {
@@ -16,10 +17,10 @@ class TypeSimpleInfoProvider {
     }
 
     private fun refineToTypeSimpleInfo(ctType: CtType<*>): TypeSimpleInfo {
-        val refinedFields = ctType.fields.map { refineToFieldSimpleInfo(it) }
+        val refinedFields = ctType.fields.map { refineToFieldSimpleInfo(it, ctType.isInterface) }
         val refinedMethods = ctType.methods
             .sortedWith(Comparator.comparing { it.position.line }) // 메서드 선언 순서대로 정렬
-            .map { refineToMethodSimpleInfo(it) }
+            .map { refineToMethodSimpleInfo(it, ctType.isInterface) }
 
         return TypeSimpleInfo(
             simpleName = ctType.simpleName,
@@ -28,20 +29,23 @@ class TypeSimpleInfoProvider {
         )
     }
 
-    private fun refineToFieldSimpleInfo(ctField: CtField<*>) =
+    private fun refineToFieldSimpleInfo(ctField: CtField<*>, isInterface: Boolean) =
         FieldSimpleInfo(
             simpleName = ctField.simpleName,
-            visibility = ctField.visibility.toString(),
+            visibility = this.processImplicitVisibility(ctField.visibility, isInterface),
             type = flattenCtTypeReference(ctField.type),
         )
 
-    private fun refineToMethodSimpleInfo(ctMethod: CtMethod<*>) =
+    private fun refineToMethodSimpleInfo(ctMethod: CtMethod<*>, isInterface: Boolean) =
         MethodSimpleInfo(
             simpleName = ctMethod.simpleName,
-            visibility = ctMethod.visibility.toString(),
+            visibility = this.processImplicitVisibility(ctMethod.visibility, isInterface),
             parameterTypeList = ctMethod.parameters.map { flattenCtTypeReference(it.type) },
             returnType = flattenCtTypeReference(ctMethod.type),
         )
+
+    private fun processImplicitVisibility(accessModifier: ModifierKind?, isInterface: Boolean): String =
+        accessModifier?.toString() ?: if (isInterface) "public" else "package"
 
     companion object GenericTypeFlattener {
         fun <T> flattenCtTypeReference(ctTypeReference: CtTypeReference<T>): String {
